e37c416d0b91fa48e63379549c686bb4ee85ea98
Julia Stevens <juliastevens02@gmail.com>
2025-09-03 15:38:34 +0200

    #9 data ophalen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<h1>Home</h1>
+<script>
+    let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
+
+    const members = data.members; 
+</script>
+
+{#each members as member}
+    <a href="/{member.id}">{member.name}</a>
+{/each}
+
+<style>
+    a {
+        display: block; 
+    }
+</style>
---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
+
+    const member = data.member; 
+</script>
+
+<a href="/">Terug naar home</a>
+<h1>Detailpagina van {member.name}</h1>
