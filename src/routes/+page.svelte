22aa60ccf03f72f24e9d107b49bd15b41a63287d
Julia Stevens <juliastevens02@gmail.com>
2025-09-04 14:49:00 +0200

    #12 #15 #16 #17 algemene styling en reset CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,8 @@
 </script>
 
 <!-- HTML -->
-<main>
 <main class="vertical-layout">
     <!-- Introductie -->
-    <section class="info">
-        <div class="title">
     <section class="info vertical-layout">
         <div class="title vertical-layout">
             <h1>Squadpage FDND</h1>
@@ -25,7 +22,6 @@
     </section>
 
     <!-- Overzicht met filters en lijst studenten -->
-    <section class="overview">
     <section class="overview vertical-layout">
         <div class="title">
             <h2>Overzicht studenten</h2>
@@ -37,19 +33,24 @@
             <ul>
                 {#each members as member}
                     <li>
-                        <article>
-                            <h3>{member.name}</h3>
-                            <a href="/{member.id}">
+                        <article class="vertical-layout">
+                            <div class="name vertical-layout">
+                                <h3>{member.name}</h3>
+                                <a href="/{member.id}">
+                                    <img 
+                                    src="link-icon.svg" 
+                                    alt="Link icoon"
+                                    class="link-icon">
+                                </a>
+                            </div>
+                            <div class="avatar">
                                 <img 
-                                src="link-icon.svg" 
-                                alt="Link icoon">
-                            </a>
-                            <img 
-                                src="{member.avatar || defaultAvatar}" 
-                                alt="Avatar van {member.name}" 
-                                width="50" 
-                                height="50" 
-                                style="object-fit:cover;">
+                                    src="{member.avatar || defaultAvatar}" 
+                                    alt="Avatar van {member.name}" 
+                                    width="50" 
+                                    height="50" 
+                                    style="object-fit:cover;">
+                            </div>
                         </article>
                     </li>
                 {/each}
@@ -59,8 +60,86 @@
 </main>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
+    * {
+        box-sizing: border-box;
+        margin: 0; 
+        padding: 0; 
+    }
+
+    main {
+        /* Kleuren */
+        --primary-background: #A675F4;
+        --secondary-background: #ECECEC;
+        --primary-highlight: #67E5BF; 
+        --primary-text: #050542; 
+        --secondary-text: #01A581; 
+
+        /* Border radius */
+        --b-radius-small: 5px; 
+        --b-radius-large: 12px; 
+
+        /* Fonts */
+        --primary-font: "Open Sans"; 
+
+        /* Padding */
+        --padding-small: 1rem .5rem; 
+        --padding-medium: 1rem 2rem; 
+        --padding-large: 3rem 2rem;
+
+        background-color: var(--secondary-background);
+        border-radius: var(--b-radius-large);
+        padding: var(--padding-large);
+
+        font-family: var(--primary-font), sans-serif;
+        font-size: 16px;
+        color: var(--primary-text);
+
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+
+    /* Reset */
+    h1 {
+        font-size: clamp(2rem, 1.4252rem + 2.4664vw, 3rem);
+    }
+
+    h2 {
+        font-size: clamp(1rem, 1.116rem + 1.7937vw, 1.5rem);
+    }
+
+    h3 {
+        font-size: 16px;
+    }
+
+    .title p {
+        font-size: clamp(1rem, 0.995rem + 1.009vw, 1.5625rem);
+    }
+
+    h1, h2, p {
+        line-height: 180%;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+    p {
+        font-weight: 300;
+    }
+
     a {
-        display: block; 
+        color: inherit;
+        text-decoration: none;
+    }
+
+    .vertical-layout {
+        display: flex; 
+        flex-direction: column; 
+        gap: 1rem;
+    }
+
     /* Styling */
     .introduction p {
         max-width: 420px;
