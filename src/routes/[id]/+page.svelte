b522cd20e0509260e5e72419b8a37b1f0a443c43
ambersr <amber.schalker@hva.nl>
2025-09-04 11:47:15 +0200

    De juiste data opgehaald #13 #23

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -6,3 +6,7 @@
 
 <a href="/">Terug naar home</a>
 <h1>Detailpagina van {member.name}</h1>
+<p>FDND jaar 2025/2026</p>
+<p>{member.squads[1].squad_id.name}</p>
+<p>{member.bio}</p>
+<img src="https://fdnd.directus.app/assets/{member.mugshot}" alt="Mugshot van {member.name}" style="width:200px; height:200px; object-fit: cover;">
