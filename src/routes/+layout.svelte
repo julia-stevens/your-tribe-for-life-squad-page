2acb7aef07c8d89cdb7ad447720e47aa82e81405
Stella <stellaclough@gmail.com>
2025-09-04 22:20:01 +0200

    Klaar gemaakt voor merge
    Titel & <p> weggegooid zodat het echte lijst goed in kan zitten

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,18 +15,8 @@
 	<header> 
 	<img class="header-logo" src="../assets/hva-blank.svg" alt="naar de homepagina">
 	<img class="header-logo" src="../assets/fdnd.png" alt="FDND">
-	<a href="/" class="home">Bekijk het programma</a>
+	<a href="https://programma.fdnd.nl/" class="home">Bekijk het programma</a>
     </header>
-
-<h1> Squadpage FDND</h1>
-<h2>Tweedejaars Studenten 2025/2026</h2>
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam,
- quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-
-<h3>Overzicht Studenten</h3>
 
 {@render children?.()}
 </main>
