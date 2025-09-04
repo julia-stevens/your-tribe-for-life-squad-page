d5688fe767b714b31d320cf9e19a5716954b6c9b
ambersr <amber.schalker@hva.nl>
2025-09-04 11:46:42 +0200

    Directus API gefilterd #13 #23

--- src/routes/[id]/+page.server.js
+++ src/routes/[id]/+page.server.js
@@ -1,5 +1,5 @@
 export async function load({ url, params }) {
-    const memberResponse = await fetch ("https://fdnd.directus.app/items/person/" + params.id);
+    const memberResponse = await fetch (`https://fdnd.directus.app/items/person/${params.id}?fields=name,bio,mugshot,squads.*.*`);
     const memberData = await memberResponse.json();
 
     return {member: memberData.data};
