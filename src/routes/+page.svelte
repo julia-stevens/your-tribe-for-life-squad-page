a2fdc6b954c1feb9fb239f329f10e0741c48ce8b
Julia Stevens <juliastevens02@gmail.com>
2025-09-04 11:24:29 +0200

    #12 #15 #16 HTML home, cards, filters, titel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,58 @@
+<!-- Data inladen -->
 <script>
     let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
 
     const members = data.members; 
+    const defaultAvatar = "default-avatar.jpg"
 </script>
 
-{#each members as member}
-    <a href="/{member.id}">{member.name}</a>
-{/each}
+<!-- HTML -->
+<main>
+    <!-- Introductie -->
+    <section class="info">
+        <div class="title">
+            <h1>Squadpage FDND</h1>
+            <p>Tweedejaars studenten 2025/2026</p>
+        </div>
+        <div class="introduction">
+            <p> 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+            </p>
+        </div>
+    </section>
+
+    <!-- Overzicht met filters en lijst studenten -->
+    <section class="overview">
+        <div class="title">
+            <h2>Overzicht studenten</h2>
+        </div>
+        <div class="filters">
+            <p>Hier komen de filters</p>
+        </div>
+        <div class="list-students">
+            <ul>
+                {#each members as member}
+                    <li>
+                        <article>
+                            <h3>{member.name}</h3>
+                            <a href="/{member.id}">
+                                <img 
+                                src="link-icon.svg" 
+                                alt="Link icoon">
+                            </a>
+                            <img 
+                                src="{member.avatar || defaultAvatar}" 
+                                alt="Avatar van {member.name}" 
+                                width="50" 
+                                height="50" 
+                                style="object-fit:cover;">
+                        </article>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </section>
+</main>
 
 <style>
     a {
