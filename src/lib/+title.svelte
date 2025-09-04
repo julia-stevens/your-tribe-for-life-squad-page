97a5c50407158aa120a68e59521568a0cabae8a1
Stella <stellaclough@gmail.com>
2025-09-04 11:42:25 +0200

    Begin van de layout
    routes gemaakt naar titles, assets & styles, en basis styles toegevoegd. Fonts werkt niet.

---
+++ src/lib/+title.svelte
@@ -0,0 +1,19 @@
+<script> 
+    export let title = "FDND";
+</script>
+
+<div class="title">
+    <h1>{title}</h1>
+</div> 
+
+<style>
+    .title {
+        text-align: center;
+        margin: 20px 0;
+    }
+
+    h1 {
+        font-size: 2.5em;
+        color: #333;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import Title from '$lib/+title.svelte';
+	import '../styles/stylesheet.css'
 
 	let { children } = $props();
 </script>
@@ -8,4 +10,27 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+
+<main>
+	<header> 
+	<img class="header-logo" src="../assets/hva-blank.svg" alt="naar de homepagina">
+	<Title title="FDND" />
+    </header>
+
+<h1> Squadpage FDND</h1>
+<h2>Tweedejaars Studenten 2025/2026</h2>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam,
+ quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+
+<h3>Overzicht Studenten</h3>
+
 {@render children?.()}
+</main>
+
+
+<footer>
+	<p>Amber, Stella & Julia | FDND 2025/2026</p>
+</footer>
