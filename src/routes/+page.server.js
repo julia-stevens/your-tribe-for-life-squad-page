0dd6932263184f8e1cdd3b60c3556cb97d066bd1
Julia Stevens <juliastevens02@gmail.com>
2025-09-11 17:46:01 +0200

    #53 content aangepast & sort functie toegevoegd

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,29 @@
 export async function load({ url }) {
-    const membersResponse = await fetch ("https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&sort=name");
+    const sort = url.searchParams.get("sort") || "name"; // default sorteren op naam
+  
+    const baseUrl = "https://fdnd.directus.app/items/person";
+    const fields =
+      "*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name";
+    const filters =
+      "filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202";
+  
+    let fetchUrl;
+  
+    if (sort === "name") {
+      fetchUrl = `${baseUrl}?fields=${fields}&${filters}&sort=name`;
+    } else if (sort === "age") {
+      fetchUrl = `${baseUrl}?fields=${fields}&${filters}&sort=birthdate`;
+    } else {
+      fetchUrl = `${baseUrl}?fields=${fields}&${filters}&sort=name`;
+    }
+  
+    const membersResponse = await fetch(fetchUrl);
     const membersData = await membersResponse.json();
-
-    return {members: membersData.data};
-}
-
-// 2E
-// https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&filter[squads][squad_id][name][_eq]=2E&sort=name
-
-// 2F
-// https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&filter[squads][squad_id][name][_eq]=2E&sort=name
-
+  
+    return {
+      members: membersData.data,
+      sort
+    };
+  }
+  
+  
