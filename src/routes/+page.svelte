babe6a75686ff0987628bff43a74b03fdd6fd621
Julia Stevens <juliastevens02@gmail.com>
2025-09-11 16:10:23 +0200

    #42 scroll driven animation cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,299 +1,389 @@
 <!-- Data inladen -->
 <script>
-    let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
+  let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
 
-    const members = data.members; 
+  const members = data.members;
+  const triggers = members.map((_, i) => `--trigger${i}`).join(", ");
 </script>
 
 <!-- HTML -->
 <main class="vertical-layout">
-    <!-- Introductie -->
-    <section class="info vertical-layout">
+  <!-- Introductie -->
+  <section class="info vertical-layout">
+    <div class="info-text vertical-layout">
         <div class="title vertical-layout">
-            <h1>Squadpage FDND</h1>
-            <p>Tweedejaars studenten 2025/2026</p>
+          <h1>Squadpage FDND</h1>
+          <p>Tweedejaars studenten 2025/2026</p>
         </div>
         <div class="introduction">
-            <p> 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
-            </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+          </p>
         </div>
-    </section>
+    </div>
+    <div class="title">
+        <h2>Overzicht studenten</h2>
+      </div>
+      <div class="filters">
+        <div class="class">
+          <p class="vertical-layout">
+            <span> Kies je klas: </span>
+            <span class="span-classes vertical-layout">
+              <a href="/">2E</a>
+              <!-- of -->
+              <a href="/" class="active-link">2F</a>
+            </span>
+          </p>
+        </div>
+      </div>
+  </section>
 
-    <!-- Overzicht met filters en lijst studenten -->
-    <section class="overview vertical-layout">
-        <div class="title">
-            <h2>Overzicht studenten</h2>
-        </div>
-        <div class="filters">
-            <div class="class">
-                <p class="vertical-layout">
-                    <span>
-                        Kies je klas: 
-                    </span>
-                    <span class="span-classes vertical-layout">
-                        <a href="/">2E</a>
-                        <!-- of -->
-                        <a href="/" class="active-link">2F</a>
-                    </span>
-                </p>
+  <!-- Overzicht met filters en lijst studenten -->
+  <section class="overview vertical-layout" style={`timeline-scope: ${triggers}`}>
+      <div 
+        class="scroll-triggers">
+            {#each members as member, i}
+            <div class="trigger" style={`view-timeline: --trigger${i}`}>
+                Trigger {i}
             </div>
-        </div>
-        <div class="list-students">
-            <ul>
-                {#each members as member}
-                    <li>
-                        <article class="vertical-layout">
-                            <div class="name vertical-layout">
-                                <h3>{member.name}</h3>
-                                <a href="/{member.id}">
-                                    <img 
-                                    src="link-icon.svg" 
-                                    alt="Link icoon"
-                                    class="link-icon">
-                                </a>
-                            </div>
-                            <div class="avatar">
-                                <img 
-                                    src="{member.avatar || "default-avatar.jpg"}" 
-                                    alt="Avatar van {member.name}" 
-                                    width="50" 
-                                    height="50" 
-                                    style="object-fit:cover;">
-                            </div>
-                        </article>
-                    </li>
-                {/each}
-            </ul>
-        </div>
-    </section>
+            {/each}
+      </div>
+      <ul>
+        {#each members as member, i}
+          <li
+            style={`
+                --z-index: ${members.length - i}; 
+                --rotate: ${i * 1.25}deg; 
+                --transformY: ${-i}%; 
+                --animation-timeline: --trigger${i}`}
+          >
+            <article class="vertical-layout">
+              <div class="name vertical-layout">
+                <h3>{member.name}</h3>
+                <a href="/{member.id}">
+                  <img 
+                    src="link-icon.svg" 
+                    alt="Link icoon" 
+                    class="link-icon" 
+                />
+                </a>
+              </div>
+              <div class="avatar">
+                <img
+                  src={member.avatar || "default-avatar.jpg"}
+                  alt="Avatar van {member.name}"
+                  width="50"
+                  height="50"
+                  style="object-fit:cover;"
+                />
+              </div>
+            </article>
+          </li>
+        {/each}
+      </ul>
+  </section>
 </main>
 
 <style>
-    * {
-        box-sizing: border-box;
-        margin: 0; 
-        padding: 0; 
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  main {
+    /* Kleuren */
+    --primary-background: #a675f4;
+    --secondary-background: #ececec;
+    --primary-highlight: #67e5bf;
+    --primary-text: #050542;
+    --secondary-text: #01a581;
+
+    /* Border radius */
+    --b-radius-small: 5px;
+    --b-radius-large: 12px;
+
+    /* Fonts */
+    --primary-font: "Open Sans";
+
+    /* Padding */
+    --padding-small: 1rem 0.5rem;
+    --padding-medium: 1rem 2rem;
+    --padding-large: 3rem 2rem;
+
+    background-color: var(--secondary-background);
+    border-radius: var(--b-radius-large);
+    padding: var(--padding-large);
+
+    font-family: var(--primary-font), sans-serif;
+    font-size: 16px;
+    color: var(--primary-text);
+
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+  /* Reset */
+  h1 {
+    font-size: clamp(2rem, 1.4252rem + 2.4664vw, 3rem);
+  }
+
+  h2 {
+    font-size: clamp(1rem, 1.116rem + 1.7937vw, 1.5rem);
+  }
+
+  h3 {
+    font-size: 16px;
+  }
+
+  .title p {
+    font-size: clamp(1rem, 0.995rem + 1.009vw, 1.5625rem);
+  }
+
+  h1,
+  h2,
+  p {
+    line-height: 180%;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  p {
+    font-weight: 300;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .vertical-layout {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  /* Styling */
+
+  /* Introduction & Text */
+  .introduction p {
+    max-width: 420px;
+  }
+
+  @media (min-width: 940px) {
+    .info-text {
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
     }
 
-    main {
-        /* Kleuren */
-        --primary-background: #A675F4;
-        --secondary-background: #ECECEC;
-        --primary-highlight: #67E5BF; 
-        --primary-text: #050542; 
-        --secondary-text: #01A581; 
-
-        /* Border radius */
-        --b-radius-small: 5px; 
-        --b-radius-large: 12px; 
-
-        /* Fonts */
-        --primary-font: "Open Sans"; 
-
-        /* Padding */
-        --padding-small: 1rem .5rem; 
-        --padding-medium: 1rem 2rem; 
-        --padding-large: 3rem 2rem;
-
-        background-color: var(--secondary-background);
-        border-radius: var(--b-radius-large);
-        padding: var(--padding-large);
-
-        font-family: var(--primary-font), sans-serif;
-        font-size: 16px;
-        color: var(--primary-text);
-
-        max-width: 1000px;
-        margin: 0 auto;
+    .title {
+      gap: 0 !important;
     }
+  }
 
-    /* Reset */
-    h1 {
-        font-size: clamp(2rem, 1.4252rem + 2.4664vw, 3rem);
+  .info {
+    background-color: var(--secondary-background);
+    padding: 1.5rem 0;
+    z-index: 9999;
+
+    @supports (animation-timeline: view()) {
+        position: sticky;
+        top: -10%;
+
+        @media (min-width: 940px) {
+            top: 20%;
+        }
     }
+  }
 
-    h2 {
-        font-size: clamp(1rem, 1.116rem + 1.7937vw, 1.5rem);
-    }
+  /* Filters */
+  .filters a {
+    margin: 1rem;
+    padding: 1rem;
+    width: min-content;
+    border: 1px solid transparent;
+    border-radius: var(--b-radius-small);
+    box-shadow: 
+            /* box shadow color */
+      -5px 5px 1px transparent,
+      /* box shadow border */ -5px 5px 0 1px transparent;
+  }
 
-    h3 {
-        font-size: 16px;
-    }
+  .filters a:hover {
+    border: 1px solid var(--primary-text);
+    border-radius: var(--b-radius-small);
+    box-shadow: 
+            /* box shadow color */
+      -5px 5px 1px var(--secondary-background),
+      /* box shadow border */ -5px 5px 0 1px var(--primary-text);
+  }
 
-    .title p {
-        font-size: clamp(1rem, 0.995rem + 1.009vw, 1.5625rem);
-    }
+  .active-link {
+    border: 1px solid var(--primary-text) !important;
+    border-radius: var(--b-radius-small);
+    box-shadow: 
+            /* box shadow color */
+      -5px 5px 1px var(--primary-highlight),
+      /* box shadow border */ -5px 5px 0 1px var(--primary-text) !important
+        ;
+    background-color: var(--primary-highlight);
+  }
 
-    h1, h2, p {
-        line-height: 180%;
-    }
-
-    ul {
-        list-style-type: none;
-    }
-
-    p {
-        font-weight: 300;
-    }
-
+  .span-classes {
+    gap: 0;
     a {
-        color: inherit;
-        text-decoration: none;
+      margin: 0 1rem;
     }
 
-    .vertical-layout {
-        display: flex; 
-        flex-direction: column; 
-        gap: 1rem;
+    @media (min-width: 310px) {
+      display: inline-block;
     }
+  }
 
-    /* Styling */
-    .introduction p {
-        max-width: 420px;
+  @media (min-width: 400px) {
+    .filters p {
+      flex-direction: row;
     }
+  }
 
-    @media (min-width: 940px) {
-        .info {
-            flex-direction: row; 
-            justify-content: space-between;
-            align-items: center;
-        }
+  /* Overview students */
+  .overview {
+    gap: 2rem;  
 
-        .title {
-            gap: 0 !important;
-        }
+    position: absolute; 
+
+  }
+
+  ul {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(15em, 1fr));
+    gap: 2rem;
+
+    @supports (animation-timeline: view()) {
+        position: sticky;
+        top: 60%;
     }
+  }
 
-    .filters a {
-        margin: 1rem;
-        padding: 1rem;
-        width: min-content;
-        border: 1px solid transparent;
-        border-radius: var(--b-radius-small);
-        box-shadow: 
+  article {
+    border: 1px solid var(--primary-text);
+    border-radius: var(--b-radius-small);
+    box-shadow: 
             /* box shadow color */
-            -5px 5px 1px transparent,
-            /* box shadow border */
-            -5px 5px 0 1px transparent
-        ; 
-    }
+      -5px 5px 1px var(--primary-highlight),
+      /* box shadow border */ -5px 5px 0 1px var(--primary-text);
+    padding: 1rem 2rem;
+    width: 60vw;
+    max-width: 255px;
+    max-height: 35vh;
+    background-color: var(--secondary-background);
+}
 
-    .filters a:hover {
-        border: 1px solid var(--primary-text);
-        border-radius: var(--b-radius-small);
-        box-shadow: 
-            /* box shadow color */
-            -5px 5px 1px var(--secondary-background),
-            /* box shadow border */
-            -5px 5px 0 1px var(--primary-text)
-        ;        
-    }
+  article .name {
+    order: 1;
+    justify-content: space-between;
+  }
 
-    .active-link {
-        border: 1px solid var(--primary-text) !important;
-        border-radius: var(--b-radius-small);
-        box-shadow: 
-            /* box shadow color */
-            -5px 5px 1px var(--primary-highlight),
-            /* box shadow border */
-            -5px 5px 0 1px var(--primary-text) !important
-        ;
-        background-color: var(--primary-highlight);
-    }
+  article .avatar {
+    width: fit-content;
+    max-width: 189px;
+    height: 189px;
+    overflow: hidden;
+  }
 
-    .span-classes {
-        gap: 0;
-         a {
-            margin: 0 1rem; 
-        }
+  article .avatar img {
+    width: 100%;
+    height: 100%;
+    border-radius: var(--b-radius-small);
+  }
 
-        @media (min-width: 310px) {
-            display: inline-block;
-        }
-    }
-
-        @media (min-width: 400px) {
-        .filters p {
-            flex-direction: row; 
-        }
-    }
-
-    .overview {
-        gap: 2rem;
-    }
-
-    ul {
-        width: fit-content;
-    }
-
-    ul {
-        width: 100%;
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(15em, 1fr));
-        gap: 2rem;
-    }
-
-    article {
-        border: 1px solid var(--primary-text);
-        border-radius: var(--b-radius-small);
-        box-shadow: 
-            /* box shadow color */
-            -5px 5px 1px var(--primary-highlight),
-            /* box shadow border */
-            -5px 5px 0 1px var(--primary-text)
-        ;
-        padding: 1rem 2rem;
-        width: 60vw;
-        max-width: 255px;
-    }
-
+  @media (min-width: 280px) {
     article .name {
-        order: 1;
-        justify-content: space-between;
+      flex-direction: row;
     }
+  }
 
-    article .avatar {
-        width: fit-content;
-        max-width: 189px;
-        height: 189px;
-        overflow: hidden;
+  /* Scroll triggers */
+  .scroll-triggers {
+    position: absolute;
+    top: 0%;
+    display: flex;
+    flex-direction: column;
+    opacity: 0; 
+  }
+
+  .scroll-triggers div {
+    width: 100%;
+    flex-grow: 1;
+    writing-mode: vertical-rl;
+    padding: 0.2em;
+    text-align: center;
+    min-height: 15vh;
+  }
+
+  /* Scroll driven animation */
+  .overview {
+    position: relative;
+    height: 1000lvh;
+  }
+
+  .overview li {
+    position: absolute;
+    z-index: var(--z-index);
+    /* rotate: var(--rotate); */
+
+    @supports (animation-timeline: view()) {
+      @media (prefers-reduced-motion: no-preference) {
+        animation-timeline: var(--animation-timeline);
+        animation-name: move-up;
+        animation-range: entry 50vh exit; /* slower scroll */
+        animation-fill-mode: both;
+      }
     }
+  }
 
-    article .avatar img {
-        width: 100%; 
-        height: 100%;
-        border-radius: var(--b-radius-small);
+  li:hover {
+    .link-icon {
+      animation: shake 0.2s ease-in 2;
     }
+  }
 
-    @media (min-width: 280px) {
-        article .name {
-            flex-direction: row;
-        }
+  @keyframes shake {
+    0%,
+    100% {
+      rotate: 0deg;
+      scale: 1;
     }
-
-    /* Animations */
-    li:hover {
-
-        .link-icon {
-            animation: shake .2s ease-in 2;
-        }
-    }
-
-    @keyframes shake {
-    0%, 100% {
-        rotate: 0deg;
-        scale: 1;
-    }    
     25% {
-        rotate: 10deg;
-        scale: 1.15;
+      rotate: 10deg;
+      scale: 1.15;
     }
     50% {
-        rotate: 0deg;
+      rotate: 0deg;
     }
     75% {
-        rotate: -10deg;
+      rotate: -10deg;
     }
-}
+  }
+
+  @keyframes move-up {
+    0% {
+      transform: translate(0%, 0%);
+      opacity: 1; 
+    }
+    75% {
+        opacity: .8; 
+    }
+    100% {
+      transform: translate(0%, -250%);
+      opacity: 0; 
+      rotate: 20deg;
+    }
+  }
 </style>
